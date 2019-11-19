6b10c09742bf5730ead0f83c722ae9349f764942
thinhphat755 <48512210+thinhphat755@users.noreply.github.com>
2019-11-19 17:06:37 +0700

    fix route index file

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
+var Product = require('../models/products');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index');
+  Product.find({})
+  .exec(function(err, listProducts){
+    if(err) {return next(err);}
+    else{
+      
+      res.render('index', {products: listProducts});
+    }
+  });
 });
 
 module.exports = router;
