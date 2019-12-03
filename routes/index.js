526e6e4e3278e4e028c72a1bef5e4bc51e0deb47
thinhphat755 <48512210+thinhphat755@users.noreply.github.com>
2019-12-03 21:44:23 +0700

    changed route index file

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,99 @@
 var express = require('express');
 var router = express.Router();
-var Product = require('../models/products');
+const bcrypt = require('bcryptjs');
+const passport = require('passport');
+const { ensureAuthenticated, forwardAuthenticated } = require('../config/auth');
+var indexController = require('../controllers/indexController');
+var usersController = require('../controllers/usersController');
+var productController = require('../controllers/productController');
+
+//User model
+const User = require('../models/user');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  Product.find({})
-  .exec(function(err, listProducts){
-    if(err) {return next(err);}
-    else{
-      
-      res.render('index', {products: listProducts});
-    }
-  });
+router.get('/', ensureAuthenticated, indexController.home);
+
+/* GET register page. */
+router.get('/register', forwardAuthenticated, usersController.register);
+
+/* GET login page. */
+router.get('/login', forwardAuthenticated, usersController.login);
+
+/*POST method register page*/
+router.post('/register', (req, res) => {
+  const {name, email, username, password, password2} = req.body;
+  let errors = [];
+
+  //Check required Fields
+  if(!name || !email || !username || !password || !password2){
+    errors.push({ msg: 'Please fill in all fields' });
+  }
+
+  //Check password match
+  if(password !== password2){
+    errors.push({ msg:'Password do not match! Please fill again!' });
+  }
+
+  //Check password length
+  if(password.length < 6){
+    errors.push({ msg: 'Password must be at least 6 characters' });
+  }
+
+  if(errors.length === 0){
+    //Validation passed
+    User.findOne({ email:email })
+    .then(user => {
+      if(user){
+        //User exists
+        errors.push({ msg: 'Email is already registered'});
+        res.render('register');
+      } else{
+        const newUser = new User({
+          name,
+          email,
+          username,
+          password
+        });
+        
+        //Hash password
+        bcrypt.genSalt(10, (err, salt) => bcrypt.hash(newUser.password, salt, (err, hash) =>
+        {
+          if(err) throw err;
+          
+          newUser.password = hash;
+
+          newUser.save()
+            .then(user => {
+              req.flash('success_msg', 'You are now registered and can log in!');
+              res.redirect('/login');
+            })
+            .catch(err => console.log(err));
+        }))
+      }
+    });
+  } else{
+    res.render('register');
+  }
 });
+
+//Login Handle
+router.post('/login', (req, res, next) => {
+  passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/login',
+    failureFlash: true
+  })(req, res, next);
+});
+
+// Logout Handle
+router.get('/logout', (req, res) => {
+  req.logout();
+  req.flash('success_msg', 'You are logged out');
+  res.redirect('/login');
+});
+
+/* GET products page*/
+// router.get('/products', productController.productList);
+// router.get('/product_detail/:id', productController.productDetail)
 
 module.exports = router;
